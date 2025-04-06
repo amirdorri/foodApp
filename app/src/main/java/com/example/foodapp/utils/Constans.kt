a3252d1b355ee554ed8243ad13396f9bc53e5094
Amir_pc <amirdorri81@gmail.com>
2025-04-06 15:25:11 +0330

    initial commit

--- app/src/main/java/com/example/foodapp/utils/Constans.kt
+++ app/src/main/java/com/example/foodapp/utils/Constans.kt
@@ -5,7 +5,6 @@ object Constants {
     const val BASE_URL = "https://www.themealdb.com/api/json/v1/1/"
     const val NETWORK_TIMEOUT = 60L
 
-    const val YOUTUBE_API_KEY = "AIzaSyDhTf2GMkCq5ktNQHjjCtsEt78Leh7vVGo"
     const val VIDEO_ID = "video_id"
 
     const val FOOD_DB_TABLE = "food_db_table"
